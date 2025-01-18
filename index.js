216e64002f67280e2a2ff434df10051fb05d7934
Bhumika12-das <bhumikadas331@gmail.com>
2025-01-18 21:26:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import bookRoute from "./route/book.route.js";
-import userRoute from "./route/user.route.js";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-dotenv.config();
-
-const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
-
-// connect to mongoDB
-try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to mongoDB");
-} catch (error) {
-    console.log("Error: ", error);
-}
-
-// defining routes
-app.use("/book", bookRoute);
-app.use("/user", userRoute);
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
